0ce2299617f8ad8b20a08f19c8810e17e2652f15
sirerr <sirerr@gmail.com>
2013-11-26 21:22:28 -0500

    got movement working again

---
+++ L1-19/X1-19 Unity/Assets/Scripts/RotateShip.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateShip : MonoBehaviour {
+
+	public float Rotationspeed = 90f;
+	public float minrotation = -45f;
+	public float maxrotation = 45f;
+	private float targetHrotation = 0;
+	private float targetVrotation = 0;
+	public float settlespeed = 0.5f;
+	private float minyrotation = -30f;
+	private float maxyrotation = 30f;
+	private float targetyrotation =0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float h = Input.GetAxis("Horizontal");
+		if(Mathf.Abs(h) >= 0.1f)
+		{
+
+			targetHrotation += -1 * h * Time.deltaTime * Rotationspeed;
+			targetHrotation = Mathf.Clamp(targetHrotation,minrotation,maxrotation);
+	
+		}
+		else {
+			targetHrotation = Mathf.Lerp(targetHrotation, 0f, Time.deltaTime / settlespeed);
+		}
+
+	    float v = Input.GetAxis("Vertical");
+	    if(Mathf.Abs(v) >= 0.1f)
+	    {
+			targetVrotation += -1 * v * Time.deltaTime * Rotationspeed;
+				targetVrotation = Mathf.Clamp(targetVrotation,minrotation,maxrotation);
+			
+		}
+		else {
+				targetVrotation = Mathf.Lerp(targetVrotation, 0f, Time.deltaTime / settlespeed);
+			}
+
+		float ry = Input.GetAxis ("Horizontal2");
+
+		if(Mathf.Abs(ry) >= 0.1f)
+		{
+			targetyrotation += -1 * ry * Time.deltaTime * Rotationspeed;
+			targetyrotation = Mathf.Clamp(targetyrotation,minyrotation,maxyrotation);
+			
+		}
+		else {
+			targetyrotation = Mathf.Lerp(targetyrotation, 0f, Time.deltaTime / settlespeed);
+		}
+		
+		Quaternion newangle = Quaternion.identity;
+		newangle.eulerAngles = new Vector3 (targetVrotation,targetyrotation,targetHrotation);
+		this.gameObject.transform.localRotation = newangle;
+
+	}
+}
